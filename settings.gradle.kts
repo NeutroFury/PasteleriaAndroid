644b09099f1865d7092a9188a327c66905c80036
Joaquin Santana <jo.santanac@duocuc.cl>
2025-11-17 13:47:49 -0300

    Modificacion de settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,21 +9,8 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        // (Opcional) repositorio de descargas de Mapbox para plugins que lo requieran
-        maven {
-            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
-            credentials {
-                username = "MAPBOX"
-                password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").orNull
-                    ?: System.getenv("MAPBOX_DOWNLOADS_TOKEN")
-            }
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
-        }
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
